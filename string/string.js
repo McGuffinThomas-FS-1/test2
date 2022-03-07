a8e263768e58e883ba0bdfe03ee40710ced927f3
Tom McGuffin <trmcguffin@student.fullsail.edu>
2022-03-06 17:20:41 -0800

    New test for the string module - all passed

---
+++ string/string.js
@@ -0,0 +1,11 @@
+const uppercase = (str) => {
+    return str.toUpperCase();
+}
+
+// lowercase function to  lowercase text
+const lowercase = (str) => str.toLowerCase()
+
+module.exports = {
+    uppercase,
+    lowercase
+}
---
+++ string/string.test.js
@@ -0,0 +1,22 @@
+const { 
+    uppercase, lowercase 
+} = require("./string")
+
+describe("Testing the string module", () => {
+
+    /*
+    test("should uppercase a string input", () => {
+        const result = uppercase("bob");
+        expect(result).toBe("BOB");
+    });
+    */
+
+    test("should uppercase a string input", () => {
+        expect(uppercase("bob")).toBe("BOB");
+    });
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULL SaiL")).toBe("full sail");
+    });
+
+});
